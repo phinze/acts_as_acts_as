4a6c0044a64255725fd812787d7956d10506d81b
phinze <paul.t.hinze@gmail.com>
2010-01-11 11:33:55 -0600

    Apparently Spec::Runner can exist w/o configure

--- lib/acts_as_acts_as.rb
+++ lib/acts_as_acts_as.rb
@@ -7,7 +7,7 @@ require 'acts_as_acts_as/group_macros'
 # Example:
 #  require 'rubygems'
 #  require 'acts_as_acts_as'
-if defined? Spec::Runner
+if defined? Spec::Runner && Spec::Runner.respond_to?(:configure)
   Spec::Runner.configure do |config|
     config.include ActsAsActsAs::Macros,      :type => :helper
     config.extend  ActsAsActsAs::GroupMacros, :type => :helper
